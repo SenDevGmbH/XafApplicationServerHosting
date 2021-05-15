03a548876309510a645586306e521e0ebe2a9839
Sergej Derjabkin <sergej@sendev.de>
2021-05-15 21:53:09 +0200

    - Added missing files

---
+++ src/UnitTests/TestObject1.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.Xpo;
+
+namespace UnitTests
+{
+    public class TestObject1 : XPBaseObject
+    {
+        public TestObject1(Session session) : base(session)
+        {
+
+        }
+
+        private int id;
+        [Key(AutoGenerate = true)]
+        public int Id
+        {
+            get { return id; }
+            set { SetPropertyValue(nameof(Id), ref id, value); }
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetPropertyValue(nameof(Name), ref name, value); }
+        }
+    }
+}
---
+++ src/UnitTests/TestObject2.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Xpo;
+
+namespace UnitTests
+{
+    [Persistent("2_TestObject2")]
+    public class TestObject2 : XPBaseObject
+    {
+        public TestObject2(Session session) : base(session)
+        {
+
+        }
+
+        private int id;
+        [Key(AutoGenerate = true)]
+        public int Id
+        {
+            get { return id; }
+            set { SetPropertyValue(nameof(Id), ref id, value); }
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetPropertyValue(nameof(Name), ref name, value); }
+        }
+    }
+}
